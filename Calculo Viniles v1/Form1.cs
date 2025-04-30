68906c89cce19cef75988ca6a9c3f9690bc90a11
Noe Gonzalez <nsebasgamer@gmail.com>
2025-04-30 15:04:00 -0600

    Version 1.0

--- Calculo Viniles v1/Form1.cs
+++ Calculo Viniles v1/Form1.cs
@@ -21,6 +21,7 @@ namespace Calculo_Viniles_v1
         private void button1_Click(object sender, EventArgs e)
         {
             try
+
             {
                 int ancho = Convert.ToInt32(textBoxAncho.Text);
                 int alto = Convert.ToInt32(textBoxAlto.Text);
