ed7a5658c9a6975e697f0e50e6dde171af8dd9d9
Noe Gonzalez <nsebasgamer@gmail.com>
2025-04-30 15:01:34 -0600

    Agregar archivos de proyecto.

---
+++ Calculo Viniles v1/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Calculo_Viniles_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarColores = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAlto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAncho = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelColores = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarColores)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBarColores
+            // 
+            this.trackBarColores.Location = new System.Drawing.Point(405, 176);
+            this.trackBarColores.Name = "trackBarColores";
+            this.trackBarColores.Size = new System.Drawing.Size(245, 45);
+            this.trackBarColores.TabIndex = 0;
+            this.trackBarColores.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(457, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Calculo Viniles";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(399, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cantidad de Colores";
+            // 
+            // textBoxAlto
+            // 
+            this.textBoxAlto.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxAlto.Location = new System.Drawing.Point(405, 271);
+            this.textBoxAlto.Name = "textBoxAlto";
+            this.textBoxAlto.Size = new System.Drawing.Size(255, 20);
+            this.textBoxAlto.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(485, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Altura";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(485, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ancho";
+            // 
+            // textBoxAncho
+            // 
+            this.textBoxAncho.Location = new System.Drawing.Point(405, 359);
+            this.textBoxAncho.Name = "textBoxAncho";
+            this.textBoxAncho.Size = new System.Drawing.Size(255, 20);
+            this.textBoxAncho.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-13, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(840, 64);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(0, 61);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(271, 439);
+            this.panel2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(24, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular Costo";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SteelBlue;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(24, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Calcular Costo Mas IVA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SteelBlue;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(24, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 53);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Calcular Costo Mayoreo";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SteelBlue;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Image = global::Calculo_Viniles_v1.Properties.Resources.info_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(24, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(220, 53);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Informacion";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // labelColores
+            // 
+            this.labelColores.AutoSize = true;
+            this.labelColores.Location = new System.Drawing.Point(502, 157);
+            this.labelColores.Name = "labelColores";
+            this.labelColores.Size = new System.Drawing.Size(54, 13);
+            this.labelColores.TabIndex = 9;
+            this.labelColores.Text = "Colores: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelColores);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxAncho);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAlto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarColores);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarColores)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarColores;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAlto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAncho;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelColores;
+    }
+}
+
---
+++ Calculo Viniles v1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calculo_Viniles_v1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ancho = Convert.ToInt32(textBoxAncho.Text);
+                int alto = Convert.ToInt32(textBoxAlto.Text);
+                int area = ancho * alto;
+                int colores = trackBarColores.Value;
+
+                double material = 55.0 * area / 6000 + 9.5 * area / 3000 + 550.0 * area / 28800;
+                double total = material * 2.5 * colores;
+
+                MessageBox.Show("El total es: " + total.ToString("C2"), "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Los Valores Ingresados No Son Numericos o No Se Han Ingresado Valores", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Los valores ingresados son demasiado Grandes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            labelColores.Text = $"Colores: {trackBarColores.Value}";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ancho = Convert.ToInt32(textBoxAncho.Text);
+                int alto = Convert.ToInt32(textBoxAlto.Text);
+                int area = ancho * alto;
+                int colores = trackBarColores.Value;
+
+                double material = 55.0 * area / 6000 + 9.5 * area / 3000 + 550.0 * area / 28800;
+                double totalIVA = material * 2.5 * colores * 1.16;
+
+                MessageBox.Show("El total con IVA es: " + totalIVA.ToString("C2"), "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Los Valores Ingresados No Son Numericos o No Se Han Ingresado Valores", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Los valores ingresados son demasiado Grandes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ancho = Convert.ToInt32(textBoxAncho.Text);
+                int alto = Convert.ToInt32(textBoxAlto.Text);
+                int area = ancho * alto;
+                int colores = trackBarColores.Value;
+
+                double material = 55.0 * area / 6000 + 9.5 * area / 3000 + 550.0 * area / 28800;
+                double totalMayoreo = material * 2 * colores;
+
+                MessageBox.Show("El total Con Precio de Mayoreo es: " + totalMayoreo.ToString("C2"), "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Los Valores Ingresados No Son Numericos o No Se Han Ingresado Valores", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Los valores ingresados son demasiado Grandes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dessarrollado por: \n\n" +
+                "Noe Gonzalez" + "\n Calculo Viniles " +
+                "\n Version 1.0" + "\n\n" +
+                "Todos los Derechos RESERVADOS., \n" +
+                "No Disponible Para la Venta");
+        }
+    }
+}
